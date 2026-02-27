844fb26e315cb20dfe4cef6af4e81bc71fefd090
rnewso4 <54243809+rnewso4@users.noreply.github.com>
2026-02-26 20:23:24 -0600

    Dialog added to add entry. Frontend only

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -9,6 +9,7 @@ import { SnackbarService, Vue3Snackbar } from "vue3-snackbar";
 import PrimeVue from 'primevue/config';
 import Material from '@primeuix/themes/material'
 import "vue3-snackbar/styles";
+import ToastService from 'primevue/toastservice';
 
 // Apply cached dark mode before first paint to prevent flash
 if (localStorage.getItem('darkMode') === 'true') {
@@ -19,6 +20,7 @@ const app = createApp(App)
 
 app.use(router)
 app.use(vuetify)
+app.use(ToastService);
 app.use(SnackbarService);
 app.use(VueFire, {
     // imported above but could also just be created here
--- vue-project/src/pages/Revenue.vue
+++ vue-project/src/pages/Revenue.vue
@@ -1,14 +1,64 @@
 <script setup>
 import RevMenu from './RevMenu.vue';
-import { Dialog, InputText, Button } from 'primevue';
+import { Dialog, InputText, Button, FloatLabel } from 'primevue';
 import { ref } from 'vue';
+import { zodResolver } from '@primevue/forms/resolvers/zod';
+import { useToast } from "primevue/usetoast";
+import { z } from 'zod';
+import { Form } from '@primevue/forms';
+import Toast from 'primevue/toast';
+
 
 const props = defineProps(['sort', 'setDialogVisible'])
 const visible = ref(false);
 
-const addFunc = () => {
-    console.log("Add clicked")
-}
+const toast = useToast();
+const initialValues = ref({
+    username: '',
+    email: ''
+});
+
+// const resolver = ref(zodResolver(
+//     z.object({
+//         username: z.string().min(1, { message: 'Username is required.' }),
+//         email: z.string().min(1, { message: 'Email is required.' }).email({ message: 'Invalid email address.' })
+//     })
+// ));
+
+const resolver = ({ values }) => {
+    const errors = {};
+
+    // if (!values.username) {
+    //     errors.username = [{ message: 'Username is required.' }];
+    // }
+
+    return {
+        values, // (Optional) Used to pass current form values to submit event.
+        errors
+    };
+};
+
+const onFormSubmit = ({ valid, values }) => {
+    if (valid) {
+        console.log(values)
+        toast.add({ severity: 'success', summary: 'Form is submitted.', life: 3000 });
+    }
+};
+
+const items = [
+    { field: 'hours', header: 'Operating Hours' },
+    { field: 'employees', header: 'Number of Employees' },
+    { field: 'spend', header: 'Daily Spend' },
+    { field: 'foot_traffic', header: 'Location Foot Traffic' },
+];
+
+const items2 = [
+    { field: 'date', header: 'Date' },
+    { field: 'price', header: 'Price' },
+    { field: 'customers', header: 'Customers' },
+    { field: 'avg_order_val', header: 'Average Order Value' },
+];
+
 </script>
 
 <template>
@@ -20,26 +70,48 @@ const addFunc = () => {
             <v-btn prepend-icon="mdi-plus-circle" variant="text" color="#CF761E" class="add-btn"
                 @click="visible = true">Add</v-btn>
             <rev-menu :sort="props.sort" :setDialogVisible="props.setDialogVisible" />
-            <Dialog v-model:visible="visible" modal header="Edit Profile" :style="{ width: '25rem' }">
-                <span class="text-surface-500 dark:text-surface-400 block mb-8">Update your information.</span>
-                <div class="flex items-center gap-4 mb-4">
-                    <label for="username" class="font-semibold w-24">Username</label>
-                    <InputText id="username" class="flex-auto" autocomplete="off" />
-                </div>
-                <div class="flex items-center gap-4 mb-8">
-                    <label for="email" class="font-semibold w-24">Email</label>
-                    <InputText id="email" class="flex-auto" autocomplete="off" />
-                </div>
-                <div class="flex justify-end gap-2">
-                    <Button type="button" label="Cancel" severity="secondary" @click="visible = false"></Button>
-                    <Button type="button" label="Save" @click="visible = false"></Button>
-                </div>
+            <Dialog v-model:visible="visible" modal header="Create Entry"
+                :style="{ width: 'fit-content', maxWidth: '90vw' }">
+                <Form v-slot="$form" :resolver="resolver" :initialValues="initialValues" @submit="onFormSubmit"
+                    class="flex flex-col gap-4 w-full sm:w-56">
+                    <div style="display: flex; width: 100%; flex-direction: row;">
+                        <div style="display: flex; flex-direction: column;">
+                            <div v-for="item of items" class="flex items-center" style="padding: 10px;">
+                                <FloatLabel variant="on">
+                                    <InputText id="on_label" :name="item.field" />
+                                    <label for="on_label">{{ item.header }}</label>
+                                </FloatLabel>
+                            </div>
+                        </div>
+                        <div style="display: flex; flex-direction: column;">
+                            <div v-for="item of items2" class="flex items-center" style="padding: 10px;">
+                                <FloatLabel variant="on">
+                                    <InputText id="on_label" :name="item.field" />
+                                    <label for="on_label">{{ item.header }}</label>
+                                </FloatLabel>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flex justify-end gap-2" id="bottomButtons">
+                        <Button type="button" label="Cancel" severity="secondary" @click="visible = false"></Button>
+                        <Button type="submit" label="Save" severity="contrast"></Button>
+                    </div>
+                </Form>
+                <Toast position="bottom-right"/>
             </Dialog>
         </div>
     </div>
 </template>
 
 <style scoped>
+#bottomButtons {
+    margin-top: 10px;
+    display: flex;
+    align-items: flex-end;
+    justify-content: center;
+    gap: 10px;
+}
+
 .add-btn {
     font-weight: bold;
     height: 100%;
