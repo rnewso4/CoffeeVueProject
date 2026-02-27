fc0e7af4f0b9b058397d4915c6d910b551175ade
rnewso4 <54243809+rnewso4@users.noreply.github.com>
2026-02-26 19:39:50 -0600

    added more data to the list

--- vue-project/src/pages/DataTable.vue
+++ vue-project/src/pages/DataTable.vue
@@ -14,19 +14,16 @@ const listData = computed(() => props.list?.value ?? props.list ?? [])
 const columns = [
     { field: 'date', header: 'Date' },
     { field: 'price', header: 'Price' },
+    { field: 'customers', header: 'Customers' },
+    { field: 'avg_order_val', header: 'Average Order Value' },
+    { field: 'hours', header: 'Operating Hours' },
+    { field: 'employees', header: 'Number of Employees' },
+    { field: 'spend', header: 'Daily Spend' },
+    { field: 'foot_traffic', header: 'Location Foot Traffic' },
 ];
 </script>
 
 <template>
-    <!-- <div class="card">
-        <DataTable :value="listData" tableStyle="min-width: 50rem" scrollable scrollHeight="400px" paginator :rows="5" :rowsPerPageOptions="[5, 10, 20, 50]">
-            <Column v-for="col of columns" :key="col.field" :field="col.field" :header="col.header" sortable>
-                <template v-if="col.field === 'price'" #body="{ data }">
-                    ${{ parseFloat(data.price).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 }) }}
-                </template>
-</Column>
-</DataTable>
-</div> -->
     <Dialog :visible="props.dialogVisible" @update:visible="(v) => props.setDialogVisible(v)" header="Revenue Table"
         :style="{ width: '75vw' }" maximizable modal :contentStyle="{ height: '60%' }">
         <DataTable :value="listData" scrollable scrollHeight="flex" tableStyle="min-width: 50rem" paginator :rows="5"
@@ -39,9 +36,6 @@ const columns = [
                 </template>
             </Column>
         </DataTable>
-        <!-- <template #footer>
-            <Button label="Ok" icon="pi pi-check" @click="props.setDialogVisible(false)" />
-        </template> -->
     </Dialog>
 
 </template>
