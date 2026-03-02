c3474d4883c69ac714949dd54f53eecf72777245
Bobby Newsome <54243809+rnewso4@users.noreply.github.com>
2026-03-02 00:00:37 -0600

    Revert "trying a new fix so the request isn't rejected"
    This reverts commit b9cc57dfb0ba08dde6778fd4bf9717fe3443d671.

--- vue-project/worker.js
+++ vue-project/worker.js
@@ -3,32 +3,20 @@ const BACKEND_ORIGIN = 'http://54.205.33.101:5000';
 export default {
   async fetch(request, env, _ctx) {
     const url = new URL(request.url);
-
-    // Proxy API requests to the backend
     if (url.pathname.startsWith('/api/')) {
       const backendUrl = BACKEND_ORIGIN + url.pathname + url.search;
-
-      const init = {
+      const backendRequest = new Request(backendUrl, {
         method: request.method,
-      };
-
-      // Forward body for non-GET/HEAD methods only
-      if (request.method !== 'GET' && request.method !== 'HEAD') {
-        init.body = request.body;
-      }
-
-      // Do NOT blindly forward browser headers such as Origin/Host/Referer,
-      // since many backends will reject those and return 403.
-      const res = await fetch(backendUrl, init);
-
+        headers: request.headers,
+        body: request.method !== 'GET' && request.method !== 'HEAD' ? request.body : undefined,
+      });
+      const res = await fetch(backendRequest);
       return new Response(res.body, {
         status: res.status,
         statusText: res.statusText,
         headers: res.headers,
       });
     }
-
-    // Everything else is a static asset request
     return env.ASSETS.fetch(request);
   },
 };
